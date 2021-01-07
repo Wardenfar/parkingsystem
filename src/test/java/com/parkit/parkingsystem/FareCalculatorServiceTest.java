67fcad2024a5b24280a14b7dd306b3ef9b604851
Wardenfar <theo.emeriau@gmail.com>
2021-01-07 17:51:33 +0100

    [UnitTests] : complate FareCalculatorService tests

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -2,17 +2,16 @@ package com.parkit.parkingsystem;
 
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.constants.ParkingType;
-import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.FareCalculatorService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FareCalculatorServiceTest {
 
@@ -30,6 +29,7 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
+    @DisplayName("Vérifie le calcul du tarif voiture correspondant à 1h")
     public void calculateFareCar() {
         Date inTime = TestUtils.parseTime("2020/01/02 08:00");
         Date outTime = TestUtils.parseTime("2020/01/02 09:00");
@@ -39,6 +39,7 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
+    @DisplayName("Vérifie le calcul du tarif vélo correspondant à 1h")
     public void calculateFareBike() {
         Date inTime = TestUtils.parseTime("2020/01/02 08:00");
         Date outTime = TestUtils.parseTime("2020/01/02 09:00");
@@ -48,6 +49,7 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
+    @DisplayName("Échec du calcul du tarif avec un type de véhicule invalide")
     public void calculateFareUnkownType() {
         Date inTime = TestUtils.parseTime("2020/01/02 08:00");
         Date outTime = TestUtils.parseTime("2020/01/02 09:00");
@@ -57,6 +59,25 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
+    @DisplayName("Échec du calcul du tarif avec une date d'arrivée vide")
+    public void calculateFareNullInTime() {
+        Date outTime = TestUtils.parseTime("2020/01/02 09:00");
+
+        assertThrows(IllegalArgumentException.class, () ->
+                fareCalculatorService.calculateFare(null, outTime, ParkingType.CAR));
+    }
+
+    @Test
+    @DisplayName("Échec du calcul du tarif avec une date de départ vide")
+    public void calculateFareNullOutTime() {
+        Date inTime = TestUtils.parseTime("2020/01/02 08:00");
+
+        assertThrows(IllegalArgumentException.class, () ->
+                fareCalculatorService.calculateFare(inTime, null, ParkingType.CAR));
+    }
+
+    @Test
+    @DisplayName("Échec du calcul du tarif vélo avec une date d'arrivée invalide")
     public void calculateFareBikeWithFutureInTime() {
         Date inTime = TestUtils.parseTime("2020/01/02 08:00");
         Date outTime = TestUtils.parseTime("1900/01/02 09:00");
@@ -66,6 +87,7 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
+    @DisplayName("Échec du calcul du tarif voiture avec une date d'arrivée invalide")
     public void calculateFareCarWithFutureInTime() {
         Date inTime = TestUtils.parseTime("2020/01/02 08:00");
         Date outTime = TestUtils.parseTime("1900/01/02 09:00");
@@ -75,6 +97,7 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
+    @DisplayName("Vérifie le tarif vélo correspondant à moins d'une heure")
     public void calculateFareBikeWithLessThanOneHourParkingTime() {
         Date inTime = TestUtils.parseTime("2020/01/02 08:00");
         Date outTime = TestUtils.parseTime("2020/01/02 08:45");
@@ -84,6 +107,7 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
+    @DisplayName("Vérifie le tarif voiture correspondant à moins d'une heure")
     public void calculateFareCarWithLessThanOneHourParkingTime() {
         Date inTime = TestUtils.parseTime("2020/01/02 08:00");
         Date outTime = TestUtils.parseTime("2020/01/02 08:45");
@@ -93,6 +117,7 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
+    @DisplayName("Vérifie le tarif voiture correspondant à plus d'un jour")
     public void calculateFareCarWithMoreThanADayParkingTime() {
         Date inTime = TestUtils.parseTime("2020/01/02 08:00");
         Date outTime = TestUtils.parseTime("2020/01/03 08:00");
@@ -102,6 +127,7 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
+    @DisplayName("Vérifie le tarif vélo correspondant à plus d'un jour")
     public void calculateFareBikeWithMoreThanADayParkingTime() {
         Date inTime = TestUtils.parseTime("2020/01/02 08:00");
         Date outTime = TestUtils.parseTime("2020/01/03 08:00");
