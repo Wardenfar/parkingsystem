5019087bde1c99096a428b4fe287d6624f94d6e9
Wardenfar <theo.emeriau@gmail.com>
2021-02-01 09:55:01 +0100

    [Fix] : little fixes

--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -57,6 +57,4 @@ public class ParkingServiceTest {
         parkingService.processExitingVehicle();
         verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
     }
-
-    //TODO
 }
